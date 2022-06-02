1318c24d40ef10e984277c4a3c77d69b6048c8fc
Alex Randolph <alexgeorgerandolph@gmail.com>
2022-06-02 11:00:21 -0600

    Test: add test for Recipes::Search index page having a search field for search recipes by name

--- Gemfile
+++ Gemfile
@@ -41,7 +41,7 @@ group :development, :test do
   gem "byebug", platforms: [:mri, :mingw, :x64_mingw]
   gem "pry"
   gem "capybara"
-  gem "faraday"
+  # gem "faraday"
   gem "simplecov"
   gem "rspec-rails"
   gem "orderly"
---
+++ spec/features/recipes/search/search_index_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+RSpec.describe "Recipe Search Page" do
+
+  describe "As a Visitor" do
+    
+    it 'On the Recipes::Search index page I see a serach field for searching a meal by name' do
+      visit '/recipes/search'
+
+      expect(page).to have_content("Search for a Recipe")
+      within("#recipes_search_name") do
+        fill_in "search for recipe by name", with: 'Chili dogs'
+        click_button "Search"
+      end 
+    end 
+
+  end 
+end 
