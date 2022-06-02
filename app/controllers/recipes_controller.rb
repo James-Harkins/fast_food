506684a7f1c7a2709def345d13e029825bdd3c63
Alex Randolph <alexgeorgerandolph@gmail.com>
2022-06-02 16:34:48 -0600

    Feat: add name identiefer to both drop down menus so that params with selection get passed to recipes#index (results) controller

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,7 +1,6 @@
 class RecipesController < ApplicationController
 
   def index
-    
   end
   
   
---
+++ spec/features/recipes/results/result_index_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+RSpec.describe "Recipe Results Index Page" do
+
+  describe "As a Visitor" do
+
+
+
+
+
+  end 
+end 
