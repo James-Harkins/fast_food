0c536562e2c8db664240575354d974533272fbd6
Alex Randolph <alexgeorgerandolph@gmail.com>
2022-06-02 16:42:50 -0600

    Feat: add logic to recipes#index if no parameter is present, redirect to /recipes/search with flash msg, add flash msg display in layouts/application.html.erb

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,6 +1,11 @@
 class RecipesController < ApplicationController
 
   def index
+    
+    if params[:area].blank? && params[:category].blank? && params[:name].blank? && params[:ingredient].blank?
+      flash[:error] = "Please Enter a Search Parameter"
+      redirect_to "/recipes/search"
+    end 
   end
   
   
--- spec/features/recipes/results/result_index_spec.rb
+++ spec/features/recipes/results/result_index_spec.rb
@@ -3,10 +3,10 @@ require 'rails_helper'
 RSpec.describe "Recipe Results Index Page" do
 
   describe "As a Visitor" do
-
+    #sad path
     it 'If I visit /recipes without having searched for a recipe first, I am redirected to /recipes/search' do
       visit '/recipes'
-
+      
       expect(page).to have_content("Please Enter a Search Parameter")
     end
 
