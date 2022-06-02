eea96eb4980ef46219ef0175f26a105633d41ecd
Alex Randolph <alexgeorgerandolph@gmail.com>
2022-06-02 16:36:40 -0600

    Test: add test for redirect/flash message if trying to visit /recipes before entering a search

--- spec/features/recipes/results/result_index_spec.rb
+++ spec/features/recipes/results/result_index_spec.rb
@@ -4,7 +4,11 @@ RSpec.describe "Recipe Results Index Page" do
 
   describe "As a Visitor" do
 
+    it 'If I visit /recipes without having searched for a recipe first, I am redirected to /recipes/search' do
+      visit '/recipes'
 
+      expect(page).to have_content("Please Enter a Search Parameter")
+    end
 
 
 
