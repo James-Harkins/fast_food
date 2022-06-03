71f877494f7799b2b292daf49e5ac2e9de95046a
James Harkins <jh.harkins@protonmail.com>
2022-06-03 16:11:42 -0600

    Refactor: update welcome page to have only a login with google button to redirect the user to google's oauth page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,6 @@ class ApplicationController < ActionController::Base
   helper_method :current_user
 
   def current_user
-    binding.pry
     UserFacade.find_by_email(session[:email]) if session[:email]
   end
 end
