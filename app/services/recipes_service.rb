936b933c133607ef52d185572b47df6f3d4a0939
Alex Randolph <alexgeorgerandolph@gmail.com>
2022-06-04 14:53:31 -0600

    Feat: add RecipesService to handle Faraday api call, call from recipesfacade

---
+++ app/services/recipes_service.rb
@@ -0,0 +1,15 @@
+class RecipesService 
+
+  def self.recipe_by_name(name)
+    conn = Faraday.new(url: "http://localhost:5000") do |f|
+      f.adapter Faraday.default_adapter
+    end
+    response = conn.get("/api/v1/recipes/search") do |c|
+      c.params[:query] = name
+    end
+    results = JSON.parse(response.body, symbolize_names: true)[:data]
+  end
+
+
+
+end 
