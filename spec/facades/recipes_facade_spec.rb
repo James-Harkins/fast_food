eb0dfb416e6c93847e00d00d4411deb44929dbcc
Alex Randolph <alexgeorgerandolph@gmail.com>
2022-06-04 18:47:28 -0600

    Test: add facade test for find recipe by ingredient

--- spec/facades/recipes_facade_spec.rb
+++ spec/facades/recipes_facade_spec.rb
@@ -2,15 +2,18 @@ require 'rails_helper'
 
 RSpec.describe RecipesFacade do
 
-  it 'Returns recipe poros for find recipe by name search', :vcr do
+  it 'Returns recipe poros for find recipes by name search', :vcr do
     recipes = RecipesFacade.find_recipes_by_name('chicken')
     expect(recipes).to be_an Array
     expect(recipes).to be_all Recipe
   end
 
+  it 'Returns recipe poros for find recipes by ingredient', :vcr do
+    recipes = RecipesFacade.find_recipes_by_ingredient('rice')
+    expect(recipes).to be_an Array
+    expect(recipes).to be_all Recipe
 
-
-
+  end 
 
 
 
