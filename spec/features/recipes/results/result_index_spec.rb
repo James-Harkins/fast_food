e1b6e84ab1af95b5465e678bd9a6c5a981a995c2
Alex Randolph <alexgeorgerandolph@gmail.com>
2022-06-04 18:45:57 -0600

    Test: uncomment test for search by ingredient

--- spec/features/recipes/results/result_index_spec.rb
+++ spec/features/recipes/results/result_index_spec.rb
@@ -73,29 +73,31 @@ RSpec.describe "Recipe Results Index Page" do
       end
     end
 
-    # it 'Has results for searching by an ingredient', :vcr do
-    #   within("#recipes_search_ingredient") do
-    #     fill_in "Search for Recipe by Single Ingredient", with: 'rice'
-    #     within("#click_button") do
-    #       click_button
-    #     end 
-    #   end 
-    #   within "#recipe_id-52997" do
-    #     expect(page).to have_content("Beef Banh Mi Bowls with Sriracha Mayo, Carrot & Pickled Cucumber")
-    #   end
+    it 'Has results for searching by an ingredient', :vcr do
+      visit '/recipes/search'
+      
+      within "#recipes_search_ingredient" do
+        fill_in "Search for Recipe by Single Ingredient", with: 'rice'
+        within "#click_button" do
+          click_button
+        end 
+      end 
+      within "#recipe_id-52997" do
+        expect(page).to have_content("Beef Banh Mi Bowls with Sriracha Mayo, Carrot & Pickled Cucumber")
+      end
 
-    #   within "#recipe_id-53054" do
-    #     expect(page).to have_content("Seri muka kuih")
-    #   end
+      within "#recipe_id-53054" do
+        expect(page).to have_content("Seri muka kuih")
+      end
 
-    #   within "#recipe_id-53008" do
-    #     expect(page).to have_content("Stuffed Lamb Tomatoes")
-    #   end
+      within "#recipe_id-53008" do
+        expect(page).to have_content("Stuffed Lamb Tomatoes")
+      end
 
-    #   within "#recipe_id-53027" do
-    #     expect(page).to have_content("Koshari")
-    #   end
-    # end 
+      within "#recipe_id-53027" do
+        expect(page).to have_content("Koshari")
+      end
+    end 
 
     # it 'Has results for selecting a recipe category', :vcr do
     #   visit '/recipes/search'
