2bb8831e93dd641b722d33b02afe7197f2fe0d33
Alex Randolph <alexgeorgerandolph@gmail.com>
2022-06-05 14:31:52 -0600

    Test: add service test for find recipe by id

--- spec/services/recipes_service_spec.rb
+++ spec/services/recipes_service_spec.rb
@@ -1,6 +1,6 @@
 require "rails_helper"
 
-describe RecipesService do
+RSpec.describe RecipesService do
   describe "class methods" do
     describe "#random_recipe" do
       it "returns data for a random recipe", :vcr do
@@ -19,5 +19,23 @@ describe RecipesService do
         expect(recipe[:attributes]).to have_key(:area)
       end
     end
+
+    it 'Returns data for a recipe by given id', :vcr do
+      recipe = RecipesService.recipe_by_id("52764")
+        expect(recipe).to be_a Hash
+        expect(recipe).to be_a Hash
+        expect(recipe).to have_key(:id)
+        expect(recipe).to have_key(:attributes)
+        expect(recipe[:attributes]).to have_key(:name)
+        expect(recipe[:attributes]).to have_key(:image)
+        expect(recipe[:attributes]).to have_key(:ingredients)
+        expect(recipe[:attributes][:ingredients]).to be_a(Hash)
+        expect(recipe[:attributes]).to have_key(:instructions)
+        expect(recipe[:attributes]).to have_key(:category)
+        expect(recipe[:attributes]).to have_key(:area)
+
+    end 
+
+
   end
 end
