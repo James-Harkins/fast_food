4a7a44fdc10ce9bad433d7e037685030fa15d955
Alex Randolph <alexgeorgerandolph@gmail.com>
2022-06-04 18:46:29 -0600

    Feat: add conditional for if params include ingredient call RecipesFacade.find_recipes_by_ingredient

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,6 +1,7 @@
 class RecipesController < ApplicationController
 
   def index
+
     if params[:area].blank? && params[:category].blank? && params[:name].blank? && params[:ingredient].blank?
       flash[:error] = "Please Enter a Search Parameter"
       redirect_to "/recipes/search"
@@ -8,8 +9,11 @@ class RecipesController < ApplicationController
     elsif params[:name]
       @query = params[:name]
       @recipes = RecipesFacade.find_recipes_by_name(@query)
+      
+    elsif params[:ingredient]
+      @query = params[:ingredient]
+      @recipes = RecipesFacade.find_recipes_by_ingredient(@query)
     end 
-
 
   
   end
--- spec/facades/recipes_facade_spec.rb
+++ spec/facades/recipes_facade_spec.rb
@@ -2,11 +2,13 @@ require 'rails_helper'
 
 RSpec.describe RecipesFacade do
 
-  it 'Returns recipe poro area for find recipe by name search', :vcr do
+  it 'Returns recipe poros for find recipe by name search', :vcr do
     recipes = RecipesFacade.find_recipes_by_name('chicken')
     expect(recipes).to be_an Array
     expect(recipes).to be_all Recipe
   end
+
+
 
 
 
