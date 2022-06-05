a310a74238b3079648e78f3c262f463b5f74866e
Alex Randolph <alexgeorgerandolph@gmail.com>
2022-06-04 18:53:10 -0600

    Feat: add RecipesService.recipes_by_ingredient to call api endpoint

--- app/facades/recipes_facade.rb
+++ app/facades/recipes_facade.rb
@@ -1,7 +1,7 @@
 class RecipesFacade
 
   def self.find_recipes_by_name(name)
-    results = RecipesService.recipe_by_name(name)
+    results = RecipesService.recipes_by_name(name)
     recipes = []
     results.each do |result|
       recipes << Recipe.new(result)
--- app/services/recipes_service.rb
+++ app/services/recipes_service.rb
@@ -1,14 +1,28 @@
 class RecipesService 
 
-  def self.recipe_by_name(name)
-    conn = Faraday.new(url: "http://localhost:5000") do |f|
+  def self.conn
+    Faraday.new(url: "http://localhost:5000") do |f|
       f.adapter Faraday.default_adapter
     end
+  end 
+
+
+  def self.recipes_by_name(name)
+    # conn = Faraday.new(url: "http://localhost:5000") do |f|
+    #   f.adapter Faraday.default_adapter
+    # end
     response = conn.get("/api/v1/recipes/search") do |c|
       c.params[:query] = name
     end
     results = JSON.parse(response.body, symbolize_names: true)[:data]
   end
+
+  def self.recipes_by_ingredient(ingredient)
+    response = conn.get("/api/v1/recipes/ingredient") do |c|
+      c.params[:query] = ingredient
+    end
+    results = JSON.parse(response.body, symbolize_names: true)[:data]
+  end 
 
 
 
