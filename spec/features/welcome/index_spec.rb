c253ec085cc0d75578762052bf7e8be8dc752c3b
James Harkins <jh.harkins@protonmail.com>
2022-06-07 16:28:32 -0600

    Feat: pass tests for authorization on landing page

--- spec/features/welcome/index_spec.rb
+++ spec/features/welcome/index_spec.rb
@@ -16,8 +16,11 @@ describe "landing page" do
 
         expect(current_path).to eq("/recipes/search")
       end
-
-      it "i see a button to login, which redirects me to the login page", :vcr do
+    end
+  end
+  describe "as a logged-in user" do
+    describe "when i visit the root path" do
+      it "i see a button to login, which redirects me to the google oauth2 page and then to my dashboard", :vcr do
         Rails.application.env_config["omniauth.auth"] = OmniAuth.config.mock_auth[:google_oauth2]
 
         visit "/"
@@ -25,6 +28,19 @@ describe "landing page" do
         click_button "Log In With Google"
 
         expect(current_path).to eq("/dashboard")
+      end
+
+      it "after logging in, if i visit the landing page, i no longer see a button to log in and i see a button to log out", :vcr do
+        Rails.application.env_config["omniauth.auth"] = OmniAuth.config.mock_auth[:google_oauth2]
+
+        visit "/"
+
+        click_button "Log In With Google"
+
+        visit "/"
+
+        expect(page).to have_button("Log Out")
+        expect(page).not_to have_button("Log In With Google")
       end
     end
   end
