7ad0d6917f01b6d0e8dcfc63e7b57f6521872d20
James Harkins <jh.harkins@protonmail.com>
2022-06-07 16:34:58 -0600

    Feat: pass tests for user dashboard to display user name, browse recipes button, and random recipe button

---
+++ spec/features/users/dashboard_spec.rb
@@ -0,0 +1,29 @@
+require "rails_helper"
+
+describe "as a logged-in user" do
+  describe "when i visit the user dashboard page" do
+    before do
+      Rails.application.env_config["omniauth.auth"] = OmniAuth.config.mock_auth[:google_oauth2]
+
+      visit "/"
+
+      click_button "Log In With Google"
+    end
+
+    it "i see my name listed", :vcr do
+      expect(page).to have_content("Example User")
+    end
+
+    it "i see a button to browse recipes, which redirects me to the /recipes/search page", :vcr do
+      click_link "Search Recipes"
+
+      expect(current_path).to eq("/recipes/search")
+    end
+
+    it "i see a button for a random recipe, which redirects me to the /recipes/search page", :vcr do
+      click_link "Random Recipe"
+
+      expect(current_path).to eq("/recipes/random")
+    end
+  end
+end
