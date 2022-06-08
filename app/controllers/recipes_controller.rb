fa14b405fb8606d89ee6f75e749ed2959f10ae11
James Harkins <jh.harkins@protonmail.com>
2022-06-08 15:16:34 -0600

    Docs: update bundle lock

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -3,18 +3,15 @@ class RecipesController < ApplicationController
     if params[:area].blank? && params[:category].blank? && params[:name].blank? && params[:ingredient].blank?
       flash[:error] = "Please Enter a Search Parameter"
       redirect_to "/recipes/search"
-
     elsif params[:name]
       @query = params[:name]
       @recipes = RecipesFacade.find_recipes_by_name(@query)
     elsif params[:ingredient]
       @query = params[:ingredient]
       @recipes = RecipesFacade.find_recipes_by_ingredient(@query)
-
     elsif params[:category]
       @query = params[:category]
       @recipes = RecipesFacade.find_recipes_by_category(@query)
-
     elsif params[:area]
       @query = params[:area]
       @recipes = RecipesFacade.find_recipes_by_area(@query)
