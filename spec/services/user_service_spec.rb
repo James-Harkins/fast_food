82a4495401ab7e524c8b0ba59fb166b5ce33503e
James Harkins <jh.harkins@protonmail.com>
2022-06-09 12:07:02 -0600

    Test: create tests for UserService#get_all_users class method

--- spec/services/user_service_spec.rb
+++ spec/services/user_service_spec.rb
@@ -19,12 +19,28 @@ describe UserService do
         email = "kaepora.co@gmail.com"
 
         user = UserService.find_by_email(email)
-        
+
         expect(user).to be_a Hash
         expect(user[:data]).to be_a Hash
         expect(user[:data][:id]).to be_a String
         expect(user[:data][:attributes][:name]).to eq("Kaepora Music")
         expect(user[:data][:attributes][:email]).to eq(email)
+      end
+    end
+
+    describe "#get_all_users" do
+      it "returns data for all users from the back-end" do
+        users = UserService.get_all_users
+
+        expect(users).to be_a Hash
+        expect(users[:data]).to be_an Array
+        expect(users[:data].count).to eq(7)
+        expect(users[:data][0]).to be_a Hash
+        expect(users[:data][0][:id]).to eq("1")
+        expect(users[:data][0][:type]).to eq("user")
+        expect(users[:data][0][:attributes]).to be_a Hash
+        expect(users[:data][0][:attributes][:name]).to eq("Frank")
+        expect(users[:data][0][:attributes][:email]).to eq("frankhdafgfad@g.com")
       end
     end
   end
