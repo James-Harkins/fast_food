be70f94f4dd70f9964f7453a8af2b9eacbc81922
James Harkins <jh.harkins@protonmail.com>
2022-06-09 12:30:35 -0600

    Feat: pass tests for UserFacde#get_all_users class method

--- app/facades/user_facade.rb
+++ app/facades/user_facade.rb
@@ -3,4 +3,9 @@ class UserFacade
     user_data = UserService.find_by_email(email)
     User.new(user_data[:data])
   end
+
+  def self.get_all_users
+    users = UserService.get_all_users[:data]
+    users.map { |user| User.new(user) }
+  end
 end
--- spec/facades/user_facade_spec.rb
+++ spec/facades/user_facade_spec.rb
@@ -3,10 +3,12 @@ require "rails_helper"
 describe UserFacade do
   describe "class methods" do
     describe "#get_all_users" do
-      users = UserFacade.get_all_users
+      it "returns user POROs for all current users in the back-end", :vcr do
+        users = UserFacade.get_all_users
 
-      expect(users).to be_all User
-      expect(users.count).to eq(7)
+        expect(users).to be_all User
+        expect(users.count).to eq(7)
+      end
     end
   end
 end
