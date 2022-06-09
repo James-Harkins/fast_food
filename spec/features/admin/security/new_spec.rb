9307126602b19e6eac6c0da753700af5fe6367aa
James Harkins <jh.harkins@protonmail.com>
2022-06-09 10:09:30 -0600

    Test: create feature tests for admin/security_check page

---
+++ spec/features/admin/security/new_spec.rb
@@ -0,0 +1,29 @@
+require "rails_helper"
+
+describe "admin/security_check page" do
+  describe "as a user" do
+    describe "when i visit the admin/security_check page" do
+      it "i see a form to submit the site password in order to proceed to login" do
+        visit "/admin/security_check"
+
+        expect(page).to have_content("Enter the Site Password to Proceed to Admin Login:")
+
+        fill_in "Site password", with: "LocalAdminTestingPassword"
+        click_button "Submit"
+
+        expect(current_path).to eq("/admin/login")
+      end
+
+      it "i see a form to submit the site password in order to proceed to login" do
+        visit "/admin/security_check"
+
+        expect(page).to have_content("Enter the Site Password to Proceed to Admin Login:")
+
+        fill_in "Site password", with: "NotTheRightPassword"
+        click_button "Submit"
+
+        expect(current_path).to eq("/admin/security_check")
+      end
+    end
+  end
+end
